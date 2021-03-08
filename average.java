543e0b323e2c2bb365ba4c20097466e132bdfbd6
yang-dtx <77356973+yang-dtx@users.noreply.github.com>
2021-03-08 13:01:26 -0600

    Add files via upload

---
+++ average.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class average {
+    public static void main(String[]args){
+System.out.print("please enter 2 numbers");
+Scanner input= new Scanner(System.in);
+System.out.println("num 1=");
+Scanner input= new Scanner(System.in);
+System.out.println("num 2=");
+Scanner input= new Scanner(System.in);
+int num = input.nextInt();
+System.out.println("your numbers are"+num + num );
+
+
+System.out.println("num 1"+"num 2");
+System.out.println("the average is "+ num1+num2/2);
+
+
+
+
+    }
+    
+    
+}
